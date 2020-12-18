6e55a5cb6770ec98fe97a0c3a3c56c50a93f4bf9
Nicholas Herrick <nth.herrick@gmail.com>
2020-12-18 12:51:04 -0700

    add working frontend login page

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -1,33 +1,41 @@
 import axios from 'axios';
-import { USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS } from "../constants/userConstants";
+import {
+  USER_LOGIN_REQUEST,
+  USER_LOGIN_SUCCESS,
+  USER_LOGIN_FAIL,
+} from '../constants/userConstants';
 
-export const login = (email, password) => async (dispatch) => {
-    try {
-        dispatch({
-            type: USER_LOGIN_REQUEST
-        });
+export const login = (email, password) => async dispatch => {
+  try {
+    dispatch({
+      type: USER_LOGIN_REQUEST,
+    });
 
-        const config = {
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        }
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
 
-        const { data } = await axios.post('/api/users/login', { email, password }, config);
+    const { data } = await axios.post(
+      '/api/users/login',
+      { email, password },
+      config
+    );
 
-        dispatch({
-            type: USER_LOGIN_SUCCESS,
-            payload: data
-        });
+    dispatch({
+      type: USER_LOGIN_SUCCESS,
+      payload: data,
+    });
 
-        localStorage.setItem('userInfo', JSON.stringify(data))
-    } catch (error) {
-        dispatch({
-            type: USER_LOGIN_FAIL,
-            payload:
-              error.response && error.response.data.message
-                ? error.response.data.message
-                : error.message,
-          });
-    }
-}
+    localStorage.setItem('userInfo', JSON.stringify(data));
+  } catch (error) {
+    dispatch({
+      type: USER_LOGIN_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
