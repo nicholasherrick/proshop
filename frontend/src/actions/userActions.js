a8c1c46c9d94b27e8828fb5dd35899c791254132
Nicholas Herrick <nth.herrick@gmail.com>
2020-12-28 19:43:55 -0700

    add place order functionality to frontend

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -153,11 +153,6 @@ export const updateUserProfile = user => async (dispatch, getState) => {
       payload: data,
     });
 
-    dispatch({
-      type: USER_LOGIN_SUCCESS,
-      payload: data,
-    });
-
     localStorage.setItem('userInfo', JSON.stringify(data));
   } catch (error) {
     dispatch({
